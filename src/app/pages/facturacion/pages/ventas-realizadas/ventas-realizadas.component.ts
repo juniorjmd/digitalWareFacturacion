ad83380c0b38b0bf2b93b5858743c922f7e5c472
jose dominguez p <juniorjmd@gmail.com>
2023-04-10 14:41:07 -0500

    agrego el punto de venta y listado de ventas realizadas

--- src/app/pages/facturacion/pages/ventas-realizadas/ventas-realizadas.component.ts
+++ src/app/pages/facturacion/pages/ventas-realizadas/ventas-realizadas.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import CustomStore from 'devextreme/data/custom_store';
+import { FacturacionService } from '../../../../services/facturacion.service';
 
 @Component({
   selector: 'app-ventas-realizadas',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./ventas-realizadas.component.scss']
 })
 export class VentasRealizadasComponent {
+  @Input() key!: number;
+  dataSource:any;
+  constructor( private sInventario:FacturacionService ){}
 
+  
+  ngAfterViewInit() {
+    this.dataSource =   new CustomStore({
+      key: 'idDocProduct',
+      load: () =>  this.sInventario.sendGetRequestLastValue(  `listadoPrdDocumento/${this.key}`)  
+    });
+  }
 }
